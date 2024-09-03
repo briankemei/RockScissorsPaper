90e53c732e26ebeb0abcc238d35bab29bdd64dcf
briankemei01@gmail.com <briankemei01@gmail.com>
2024-09-02 23:22:13 -0400

    Refactored the Rock, Paper, Scissors game into a modular structure. Split the code into two separate files: a functions file (RSPF.py) and a main program file. The functions handling user input, computer selection, and game logic were moved to the functions file. The main program imports these functions to manage the game loop and flow. This enhances code readability, modularity, and maintainability.

--- RSP.py
+++ RSP.py
@@ -1,5 +1,5 @@
-# Importing the random module to generate random choices for the computer
 import random
+import RSPF as selection
 
 print(" Play the game by choosing Rock, Scissors or Paper ")
 
@@ -10,49 +10,18 @@ count = 1
 while True:
     # Adding the number of times the user played
     count += 1
-    # List of choice a computer will pick on
-    select = ["Rock", "Scissors", "Paper"]
-    opponent = random.choice(select)
 
-    # Getting user input and capitalizing it for consistency
-    user = input('What is your move? ').capitalize()
+    user_move = selection.user_choice()
+    computer_move = selection.computer_choice()
 
-    # Check for a tie condition
-    if user == opponent:
-        print("It's a tie!\n")
-
-    # Check if user chose Rock
-    elif user == "Rock":
-        if opponent == "Scissors":
-            print("Rock smashes scissors!\n\t You win!!!\n")
-        else:
-            print("Rock covers paper!\n\t You lost!!!\n")
-
-    # Check if user chose Scissors
-    elif user == "Scissors":
-        if opponent == "Paper":
-            print("Scissors cuts paper!\n\t You win!!!\n")
-        else:
-            print("paper covers Rock!\n\t You lost!!!\n")
-
-    # Check if user chose paper
-    elif user == "Paper":
-        if opponent == "Rock":
-            print("Paper covers Rock!\n\t You win!!!\n")
-        else:
-            print("Rock smashes Scissors!\n\t You lost!!!\n")
-
-    # If input from user is invalid
-    else:
-        print("Invalid choice\n")
-
-    # Ask the user if they want to continue playing the game.
-    play = input("Do you want to play again? (Y/N): ").lower()
+    selection.play_RSP(user_move, computer_move)
+    play = selection.play_again()
 
     # If the user does not choose y  the game will terminate
     if play != "y":
-        print(f"Game over. You played {count-1} times.")
-        break # the loop will break ending the game
+        print(f"Game over. You played {count - 1} times.")
+        break  # the loop will break ending the game
     else:
         print(f"Another round of play, game number {count}.\n")
+
 
---
+++ RSPF.py
@@ -0,0 +1,57 @@
+''' Code the fucnctions here'''
+import random
+
+
+# Function to get the computer choice
+
+def computer_choice():
+    # List of choice a computer will pick on
+    select = ["Rock", "Scissors", "Paper"]
+    opponent = random.choice(select)
+    return opponent
+
+
+# Function to get players choice
+def user_choice():
+    # Getting user input and capitalizing it for consistency
+    user = input('What is your move? ').capitalize()
+    return user
+
+
+def play_RSP(user_choice, computer_choice):
+    # Check for a tie condition
+    if user_choice == computer_choice:
+        print("It's a tie!\n")
+
+    # Check if user chose Rock
+    elif user_choice == "Rock":
+        if computer_choice == "Scissors":
+            print("Rock smashes scissors!\n\t You win!!!\n")
+        else:
+            print("Rock covers paper!\n\t You lost!!!\n")
+
+    # Check if user chose Scissors
+    elif user_choice == "Scissors":
+        if computer_choice == "Paper":
+            print("Scissors cuts paper!\n\t You win!!!\n")
+        else:
+            print("paper covers Rock!\n\t You lost!!!\n")
+
+    # Check if user chose paper
+    elif user_choice == "Paper":
+        if computer_choice == "Rock":
+            print("Paper covers Rock!\n\t You win!!!\n")
+        else:
+            print("Rock smashes Scissors!\n\t You lost!!!\n")
+
+    # If input from user is invalid
+    else:
+        print("Invalid choice\n")
+
+#functio to ask
+def play_again ():
+    # Ask the user if they want to continue playing the game.
+    play = input("Do you want to play again? (Y/N): ").lower()
+    return play
+
+
